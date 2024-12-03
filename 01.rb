cd79b1fca4d49a9ec648baa5d40672916fe8ca6f
Sofia Besenski <sofia@super.gd>
2024-12-02 23:06:38 -0800

    First pass implementation

--- 01.rb
+++ 01.rb
@@ -4,23 +4,39 @@ require_relative "./shared"
 
 INPUT = load_input(__FILE__).chomp
 
+def diff(a, b)
+  (a - b).abs
+end
+
 def part_one(input)
+  list_1 = []
+  list_2 = []
+
+  input.split("\n").each do |pair|
+    first_list_element, second_list_element = pair.split
+
+    list_1 << first_list_element.to_i
+    list_2 << second_list_element.to_i
+  end
+
+  list_1.sort.zip(list_2.sort).sum { |first, second|
+    diff(first,second)
+  }
 end
 
 def part_two(input)
 end
 
 puts "Solution for part one: #{part_one(INPUT).inspect}" unless ENV["PART"] == "2"
-puts "Solution for part two: #{part_two(INPUT).inspect}" unless ENV["PART"] == "1"
+# puts "Solution for part two: #{part_two(INPUT).inspect}" unless ENV["PART"] == "1"
 
 RSpec.describe "the solution" do
   describe "#part_one" do
     subject { part_one input }
-    let(:input) { "47078   87818\n99261   15906\n44723   23473\n87598   26876" }
 
-    it 'returns a number' do
-      expect(subject).to be_a(Integer)
-    end
+    let(:input) { "1   3\n5   2" }
+
+    it {is_expected.to eq 3}
   end
 
   describe "#part_two" do
