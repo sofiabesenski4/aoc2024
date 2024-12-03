633125b6cd95c4fc17b0db281d2855a9d7b7f631
Sofia Besenski <sofia@super.gd>
2024-12-02 22:52:20 -0800

    Failing spec

--- 01.rb
+++ 01.rb
@@ -16,6 +16,11 @@ puts "Solution for part two: #{part_two(INPUT).inspect}" unless ENV["PART"] == "
 RSpec.describe "the solution" do
   describe "#part_one" do
     subject { part_one input }
+    let(:input) { "47078   87818\n99261   15906\n44723   23473\n87598   26876" }
+
+    it 'returns a number' do
+      expect(subject).to be_a(Integer)
+    end
   end
 
   describe "#part_two" do
--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source "https://rubygems.org"
 
 ruby '3.3.3'
 
+gem 'pry'
 gem 'rspec'
 
 # gem "rails"
