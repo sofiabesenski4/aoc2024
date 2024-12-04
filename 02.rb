7a7bd9cccb6ac997c13c2ecdca0a1036fc7f6627
Sofia Besenski <sofia@super.gd>
2024-12-03 19:58:24 -0800

    02 Part 2

--- 02.rb
+++ 02.rb
@@ -30,16 +30,11 @@ end
 
 def part_two(input)
   input.split("\n").sum { | report |
-    report = report.split.map!{_1.to_i}
+    report = report.split.map(&:to_i)
 
-    safe = 1
-
-    safe = 0 unless (ordered?(list: report, reverse: true) || ordered?(list: report, reverse: false))
-
-    report.each_cons(2){ |a, b| safe = 0 unless within_diff?(a,b) }
-
-    safe
-  } 
+    report.combination(report.length - 1)
+      .any?{ |report_subset| check_report(report_subset) == 1 } ? 1 : 0
+  }
 end
 
 puts "Solution for part one: #{part_one(INPUT).inspect}" unless ENV["PART"] == "2"
@@ -50,13 +45,17 @@ RSpec.describe "the solution" do
     subject { part_one(input) }
     let(:input) { "1 3 5 7\n4 5 1 0\n9 5 4 1\n9 8 7 6" }
 
-    it "" do
-        binding.pry
-    end
     it {is_expected.to eq(2)}
   end
 
   describe "#part_two" do
+    it "correctly marks a report as safe" do
+      expect(part_two("1 3 5 7")).to eq(1)
+    end
+
+    it "will recognize an unsafe report" do
+      expect(part_two("10 5 1")).to eq(0)
+    end
   end
 end
    
