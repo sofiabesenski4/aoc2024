4d6257abdd1db16ec2128095e144c6c1a57ff479
Sofia Besenski <sofia@super.gd>
2024-12-05 20:05:35 -0800

    05 initial commit

---
+++ 05.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require_relative "./shared"
+
+INPUT = load_input(ENV["INPUT"] || __FILE__).chomp
+
+def part_one(input)
+end
+
+def part_two(input)
+end
+
+# puts "Solution for part one: #{part_one(INPUT).inspect}" unless ENV["PART"] == "2"
+# puts "Solution for part two: #{part_two(INPUT).inspect}" unless ENV["PART"] == "1"
+
+RSpec.describe "the solution" do
+  describe "#part_one" do
+    let(:input) { 
+    }
+  end
+
+  describe "#part_two" do
+  end
+end
+   
+
+
+
